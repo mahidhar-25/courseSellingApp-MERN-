2944ae3cf99a0bfd8da8024f8b08a58aca854863
mahidhar reddy <mahidhar100008@gmail.com>
2024-01-27 11:06:14 +0530

    added material tailwind react library

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -1,33 +1,70 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+  Tooltip,
+  IconButton,
+} from "@material-tailwind/react";
+import { Progress } from "@material-tailwind/react";
+
 import React from "react";
-const progressPercentage = 60;
+
 function CourseCard({ imageUrl, title, description, rating }) {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col mx-3 my-3  basis-1/5">
-      {console.log(title)}
-      <img className="w-full" alt="course image " src={imageUrl} />
-
-      <div className="mt-4 mb-3 p-2">
-        <h3 className="font-bold text-xl mb-2">{title}</h3>
-        <p className="text-gray-700 text-base"> {description}</p>
-      </div>
-      <div className="h-1 w-full bg-gray-300 mb-4 pe-2">
-        <div
-          style={{ width: `${progressPercentage}%` }}
-          className={`h-full ${
-            progressPercentage < 70 ? "bg-red-600" : "bg-green-600"
-          }`}
-        ></div>
-        <div className="flex flex-row justify-between">
-          <p className="text-sm">{progressPercentage}% complete</p>
-          <p className="text-sm"> {rating}/5</p>
+    <Card className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col mx-3 my-3">
+      <CardHeader floated={false} color="blue-gray">
+        <img width="300" height="400" src={imageUrl} alt="image course" />
+      </CardHeader>
+      <CardBody>
+        <div className="mb-3 flex items-center justify-between">
+          <Typography variant="h4" color="blue-gray" className="font-medium">
+            {title}
+          </Typography>
+          <Typography
+            color="blue-gray"
+            className="flex items-center gap-1.5 font-normal"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="-mt-0.5 h-5 w-5 text-yellow-700"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                clipRule="evenodd"
+              />
+            </svg>
+            {rating}
+          </Typography>
         </div>
-      </div>
-      <div className="w-4/5 p-2 flex flex-col items-center justify-center m-auto">
-        <button className="bg-blue-700 rounded-full text-white text-center w-full p-2 mb-2 ">
-          go to course
-        </button>
-      </div>
-    </div>
+        <Typography color="gray">{description}</Typography>
+        <div className="w-full">
+          <div className="mb-2 flex items-center justify-between gap-4">
+            <Typography color="blue-gray" variant="h6">
+              Completed
+            </Typography>
+            <Typography color="blue-gray" variant="h6">
+              50%
+            </Typography>
+          </div>
+          <Progress value={50} size="sm" />
+        </div>
+      </CardBody>
+      <CardFooter className="pt-3">
+        <Button
+          size="lg"
+          fullWidth={true}
+          className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+          Go to course
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
 
---
+++ frontend/src/components/CourseCard2.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+const progressPercentage = 60;
+function CourseCard({ imageUrl, title, description, rating }) {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col mx-3 my-3  basis-1/5">
+      {console.log(title)}
+      <img className="w-full" alt="course image " src={imageUrl} />
+
+      <div className="mt-4 mb-3 p-2">
+        <h3 className="font-bold text-xl mb-2">{title}</h3>
+        <p className="text-gray-700 text-base"> {description}</p>
+      </div>
+      <div className="h-1 w-full bg-gray-300 mb-4 pe-2">
+        <div
+          style={{ width: `${progressPercentage}%` }}
+          className={`h-full ${
+            progressPercentage < 70 ? "bg-red-600" : "bg-green-600"
+          }`}
+        ></div>
+        <div className="flex flex-row justify-between">
+          <p className="text-sm">{progressPercentage}% complete</p>
+          <p className="text-sm"> {rating}/5</p>
+        </div>
+      </div>
+      <div className="w-4/5 p-2 flex flex-col items-center justify-center m-auto">
+        <button className="bg-blue-700 rounded-full text-white text-center w-full p-2 mb-2 ">
+          go to course
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CourseCard;

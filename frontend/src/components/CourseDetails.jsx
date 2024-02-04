50003ca26f1322446535fcbe8bc360d48a85bbb8
mahidhar reddy <mahidhar100008@gmail.com>
2024-02-04 22:40:49 +0530

    hero page for specific course is updated

--- frontend/src/components/CourseDetails.jsx
+++ frontend/src/components/CourseDetails.jsx
@@ -4,6 +4,7 @@ import { filteredAllCourses } from "../store/atoms/courses";
 import { useRecoilValue } from "recoil";
 import NavItems from "./NavItems";
 import { loading } from "../store/atoms/nav";
+import { Rating, Typography } from "@material-tailwind/react";
 
 function CourseDetails() {
   const { courseTitle } = useParams();
@@ -26,30 +27,91 @@ function CourseDetails() {
     return JSON.stringify(course);
   };
 
+  const getDateMonthYearFormatString = useCallback((dateString) => {
+    const dateObject = new Date(dateString);
+    const month = dateObject.getUTCMonth() + 1; // Adding 1 because months are zero-indexed
+    const year = dateObject.getUTCFullYear();
+    const day = dateObject.getUTCDate();
+
+    const newDate = `${day}/${month}/${year}`;
+    return newDate;
+  }, []);
   return (
     <div>
       <NavItems />
       {!loadingValue && (
         <div className="h-50v bg-gray-800 text-white ">
-          <div className="h-full p-5 flex flex-row justify-around  content-center items-center">
+          <div className="h-full px-5 py-2 flex flex-row justify-around content-center items-center">
             {course && (
               <>
                 <div className="items-center">
-                  <div>
+                  <div className="mx-auto">
                     <h1 className="text-6xl font-semibold hover:font-bold cursor-pointer">
                       {course.title}
                     </h1>
-                    <p>{course.description}</p>
+                    <p className="my-3 text-wrap">
+                      {course.description.length > 300
+                        ? course.description.slice(0, 300) + "..."
+                        : course.description}
+                    </p>
+                    <div className="flex items-center gap-2 font-bold text-white m-2">
+                      {course.details.rating}
+
+                      <Rating
+                        value={Math.floor(course.details.rating)}
+                        readonly
+                        className="my-2"
+                      />
+                      <Typography
+                        color="white"
+                        className="font-medium text-white-gray-500"
+                      >
+                        <span
+                          className=" text-blue-400 
+                   hover:text-rose-900 text-sm my-2"
+                        >
+                          ({course.details.noOfPeopleRated} ratings)
+                        </span>
+                        {course.details.noOfPeoplePurchased}students
+                      </Typography>
+                    </div>
+
+                    <p className="my-2 text-md">
+                      price :{" "}
+                      <span className="text-bold text-yellow-200">
+                        ${course.price}
+                      </span>
+                    </p>
+                    <p className="my-2">
+                      creator :{" "}
+                      <a
+                        href="#"
+                        className="underline text-blue-400 
+                   hover:text-rose-900 
+                   visited:text-rose-700"
+                      >
+                        {course.creatorUsername}
+                      </a>
+                    </p>
+                    <p className="text-sm my-2">
+                      <span>
+                        last updated{" "}
+                        {getDateMonthYearFormatString(
+                          course.details.lastUpdatedOn
+                        )}
+                        ,
+                      </span>
+                      <span className="mx-3 ">
+                        {""}
+                        created on{" "}
+                        {getDateMonthYearFormatString(course.details.createdOn)}
+                      </span>
+                    </p>
                   </div>
                 </div>
                 <div className="justify-center content-center ">
-                  <div>
-                    <img
-                      height="300"
-                      width="400"
-                      src={course.image}
-                      alt="image course"
-                    />
+                  <div className="">
+                    <img src={course.image} alt="image course" />
                   </div>
                 </div>
               </>
